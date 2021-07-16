d2da2fee143cc354d2652283e98203e5db2751ac
Armin <arminb997@gmail.com>
2021-07-16 09:59:14 +0200

    Implement end date countdown
    If there's less than 30 minutes left until the article offer ends
    a countdown in red will be shown, indicating how much time is left
    until the article expires.

--- src/components/article-card/ArticleCard.tsx
+++ src/components/article-card/ArticleCard.tsx
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./article-card.scss";
 import { Link } from "react-router-dom";
 import { SearchArticle } from "../../common/types";
 
 const ArticleCard: React.FC<SearchArticle> = ({ id, imageUrl, title, endDate, buyNowPrice }) => {
-  const dateString = new Date(endDate).toLocaleString("de-DE", {
-    minute: "numeric",
-    hour: "numeric",
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  });
+  const [dateString, setDateString] = useState("");
+  const [endsSoon, setEndsSoon] = useState(false);
+
+  const returnDateString = () => {
+    return new Date(endDate).toLocaleString("de-DE", {
+      minute: "numeric",
+      hour: "numeric",
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+  };
+
+  let intervalId: any;
+
+  useEffect(() => {
+    const difference = Date.parse(endDate) - Date.now();
+
+    // If there's less than 30 min left until the article offer ends, start a countdown
+    if (difference < 1800000) {
+      setEndsSoon(true);
+      initializeCountDown();
+    } else {
+      setDateString(returnDateString());
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const initializeCountDown = () => {
+    intervalId = setInterval(() => {
+      const difference = Date.parse(endDate) - Date.now();
+      const differenceDate = new Date(difference);
+      if (difference <= 1000) {
+        clearInterval(intervalId);
+      }
+
+      const minutes = ("0" + differenceDate.getMinutes()).slice(-2);
+      const seconds = ("0" + differenceDate.getSeconds()).slice(-2);
+      setDateString(`ends in ${minutes}:${seconds}`);
+    }, 1000);
+  };
 
   return (
     <Link to={`/article/${id}`} id="article-card-container">
@@ -18,7 +55,7 @@ const ArticleCard: React.FC<SearchArticle> = ({ id, imageUrl, title, endDate, bu
         <img src={imageUrl} alt="Product" />
       </div>
       <p className="title">{title}</p>
-      <p className="end-date">{dateString}</p>
+      <p className={`${endsSoon && "ends-soon"} end-date`}>{dateString}</p>
       <p className="price">
         {buyNowPrice !== null && (
           <>
